ce7bfea396a847cbc83576566fc5a0c46f0fe50a
oscar <oscarmancilla227@gmail.com>
2025-05-19 10:37:56 -0600

    primer commit

---
+++ EjerciciosRazorPages/Pages/IMC.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjerciciosRazorPages.Pages
+{
+    public class IMCModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
